4ac6f9aa5d628e2c91b02a8740fb15e506689ca8
zhangjh <zhangjh@zhangjh.stt.com>
2018-05-15 10:38:35 +0800

    initial

---
+++ pomp-auto/src/com/stt/main/HelloTest.java
@@ -0,0 +1,18 @@
+package com.stt.main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.TestNG;
+
+public class HelloTest {
+	public  static void main(String[] args) {
+		TestNG testng = new TestNG();
+		List<String> suitefiles = new ArrayList<String>();
+		suitefiles.add("config/testsuite.xml");
+		testng.setTestSuites(suitefiles);
+		testng.run();
+	}
+}
---
+++ pomp-auto/src/com/stt/pomp/testng/PompLogin.java
@@ -0,0 +1,84 @@
+package com.stt.pomp.testng;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class PompLogin {
+	private WebDriver driver;
+	  private String baseUrl;
+	  private boolean acceptNextAlert = true;
+	  private StringBuffer verificationErrors = new StringBuffer();
+
+	  @BeforeClass(alwaysRun = true)
+	  public void setUp() throws Exception {
+	    driver = new FirefoxDriver();
+	    baseUrl = "http://192.168.1.208/pomp";
+	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  }
+	 
+	  @Test
+	  public void testDemo1() throws Exception {
+	    driver.get(baseUrl + "/user/login.do");
+	   
+	    assertEquals(driver.findElement(By.cssSelector("td.login")).getText(), "用户名：");
+	  }
+
+	  @Test
+	  public void testDemo2() throws Exception {
+	    driver.get(baseUrl + "/user/login.do");
+	    assertEquals(driver.getTitle(), "管理员登录");
+	    
+	  }
+	  
+	  @AfterClass(alwaysRun = true)
+	  public void tearDown() throws Exception {
+	    driver.quit();
+	    String verificationErrorString = verificationErrors.toString();
+	    if (!"".equals(verificationErrorString)) {
+	      fail(verificationErrorString);
+	    }
+	  }
+
+	  private boolean isElementPresent(By by) {
+	    try {
+	      driver.findElement(by);
+	      return true;
+	    } catch (NoSuchElementException e) {
+	      return false;
+	    }
+	  }
+
+	  private boolean isAlertPresent() {
+	    try {
+	      driver.switchTo().alert();
+	      return true;
+	    } catch (NoAlertPresentException e) {
+	      return false;
+	    }
+	  }
+
+	  private String closeAlertAndGetItsText() {
+	    try {
+	      Alert alert = driver.switchTo().alert();
+	      String alertText = alert.getText();
+	      if (acceptNextAlert) {
+	        alert.accept();
+	      } else {
+	        alert.dismiss();
+	      }
+	      return alertText;
+	    } finally {
+	      acceptNextAlert = true;
+	    }
+	  }
+}
